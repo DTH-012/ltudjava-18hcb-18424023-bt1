695c8083d6b9410e5f7c0315bb9e14b7e38451f4
htdat0202 <htdat0202@gmail.com>
2019-07-28 22:18:10 +0700

    File reader

---
+++ src/FileData.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileData {
+	public FileData() {
+		super();
+	}
+	
+	public LopHoc readDSSV(String path) throws Exception{
+		LopHoc lopHoc = new LopHoc();
+		File file = new File(path);
+		if(file.exists()) {
+			Scanner s = new Scanner(file);
+			lopHoc.setTenLop(file.getName().replaceFirst("[.][^.]+$", ""));
+			while(s.hasNextLine()) {
+				String line = s.nextLine();
+				String[] data = line.split(",");
+				SinhVien sv = new SinhVien(Integer.parseInt(data[0]),data[1],data[2],data[3]);
+				if(!lopHoc.ThemSV(sv)) {
+					return null;
+				}
+			}
+		}
+		
+		return lopHoc;
+	}
+	
+	public <T> boolean writeData(String pathFolder,String fileName, ArrayList<T> DataList) throws IOException {
+		FileWriter fw;
+		try 
+		{
+			fw = new FileWriter(pathFolder+fileName+".csv");
+		}
+		catch(IOException exc) 
+		{
+			return false;
+		}
+		
+		for(T data : DataList) {
+			String str = data.toString() + "\r\n";
+			fw.write(str);
+		}
+		
+		fw.close();
+		return true;
+	}
+}
